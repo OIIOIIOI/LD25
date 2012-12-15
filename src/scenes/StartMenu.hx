dbb903767258c1e0122d85fca128c86fc9a669f2
Grmpf <thibgoupy@orange.fr>
2012-12-15 11:19:48 +0100

    vkdc

--- src/scenes/StartMenu.hx
+++ src/scenes/StartMenu.hx
@@ -2,7 +2,6 @@ package scenes;
 
 import events.EventManager;
 import events.GameEvent;
-import flash.display.Sprite;
 import flash.events.MouseEvent;
 import Game;
 import scenes.Scene;
@@ -15,17 +14,16 @@ import scenes.Scene;
 class StartMenu extends Scene
 {
 	
-	private var m_test:Sprite;
+	private var btnGraphics:CREDITBTN;
 	
 	public function new () {
 		super();
-		
-		m_test = new Sprite();
+		/*m_test = new Sprite();
 		m_test.graphics.beginFill(0x000000);
 		m_test.graphics.drawRect(0, 0, 400, 200);
 		m_test.graphics.endFill();
 		m_test.buttonMode = true;
-		addChild(m_test);
+		addChild(m_test);*/
 	}
 	
 	override private function clickHandler (_event:MouseEvent) :Void {
