ed7e09c3d879022153264bc2a03608b60e599cfe
01101101 <01101101@proceduralactivity.com>
2012-12-16 18:21:25 +0100

    font library

--- src/scenes/Scores.hx
+++ src/scenes/Scores.hx
@@ -4,6 +4,7 @@ import events.EventManager;
 import events.GameEvent;
 import flash.display.Sprite;
 import flash.events.MouseEvent;
+import flash.text.AntiAliasType;
 import flash.text.TextField;
 import flash.text.TextFormat;
 import Game;
@@ -19,15 +20,15 @@ class Scores extends Scene
 	private var backbtn:BACKBTN;
 	private var scoreDisplay :TextField;
 
-	public function new() 
+	public function new()
 	{
 		super();
 		scorebg = new SCOREBG();
 		backbtn =  new BACKBTN();
 		scoreDisplay = new TextField();
 		
-		var scoreformat = new TextFormat();
-		scoreformat.font = "True Crimes";
+		//var scoreformat = new TextFormat("GoodDog", 32);
+		var scoreformat = new TextFormat("TrueCrimes", 32);
 		addChild(scorebg);
 		addChild(backbtn);
 		addChild(scoreDisplay);
@@ -35,11 +36,16 @@ class Scores extends Scene
 		scoreDisplay.y = 100;
 		scoreDisplay.width = 300;
 		scoreDisplay.height = 300;
+		scoreDisplay.border = true;
 		scoreDisplay.embedFonts = true;
-		scoreDisplay.defaultTextFormat=scoreformat;
+		scoreDisplay.selectable = false;
+		scoreDisplay.antiAliasType = AntiAliasType.ADVANCED;
+		scoreDisplay.defaultTextFormat = scoreformat;
+		scoreDisplay.text = "dfghjklmiuytr\n";
 		for (i in ScoreManager.scoreBoard) {
 			scoreDisplay.text += "" + i.name + "     " + i.score+"\n";
 		}
+		scoreDisplay.setTextFormat(scoreformat);
 	}
 	
 	override private function clickHandler (_event:MouseEvent) :Void {
